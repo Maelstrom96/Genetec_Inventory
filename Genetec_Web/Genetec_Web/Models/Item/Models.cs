34490f7f8fc50c2bc2c51093791f9a509a8ad247
Alexandre-Jacques St-Jacques <alexandrovich8@live.ca>
2016-06-12 18:14:31 -0400

    rename & structure

---
+++ Genetec_Web/Genetec_Web/Models/Item/Models.cs
@@ -0,0 +1,43 @@
+﻿using Genetec_Web.Models.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Genetec_Web.Models.Item
+{
+    public class Models
+    {
+        List<Model> List = new List<Model>();
+
+        public Models()
+        {
+            Import();
+        }
+
+        private void Import()
+        {
+
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Modify()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
